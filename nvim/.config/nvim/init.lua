0111a08db7c0d3bbb6ebf9bc37c19aa0bbf2662c
Omer Tatar <majimomi@gmail.com>
2024-06-25 15:31:37 +0200

    first commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,36 @@
+
+vim.fn.system({
+  "git",
+  "config",
+  "--global",
+  "http.sslVerify",
+  "false"
+})
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "http://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("vim-options")
+require("lazy").setup("plugins")
+
+
+
+
+
+
+
+
+
+
+
+
+
