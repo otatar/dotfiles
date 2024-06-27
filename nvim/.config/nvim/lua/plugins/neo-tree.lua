4ad4410367a82758e74743074cdaef3da7501358
Omer Tatar <majimomi@gmail.com>
2024-06-27 14:40:21 +0200

    added autotag

--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -6,14 +6,17 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
-	opts = {
-		filesystem = {
-			filtered_items = {
-				visible = true,
-			},
-		},
-	},
 	config = function()
+    require('neo-tree').setup({
+      filesystem = {
+        filtered_items = {
+          visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+          hide_dotfiles = false,
+         },
+      }
+  })
 		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal left<CR>")
+    vim.api.nvim_create_autocmd({"VimEnter"},  { command = "Neotree toggle" })
+
 	end,
 }
